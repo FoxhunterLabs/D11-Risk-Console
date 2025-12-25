e57ee6b537ebc4abda4f843a7811f12ec82289b5
Foxhunter Labs <joewells636@gmail.com>
2025-12-25 00:04:21 -0500

    Add SENSOR_KEYS and ML_FEATURES lists
    Define SENSOR_KEYS and ML_FEATURES for machine learning.

---
+++ ml/features.py
@@ -0,0 +1,15 @@
+SENSOR_KEYS = [
+    "speed_kph", "gear", "engine_rpm", "grade_pct",
+    "roll_deg", "pitch_deg", "blade_load_pct",
+    "fuel_pct", "ground_firmness", "moisture_index",
+    "visibility", "gnss_hdop", "gnss_jitter_m",
+    "obstacle_distance_m", "obstacle_bearing_deg",
+    "shift_hours", "micro_corrections_per_min",
+]
+
+ML_FEATURES = [
+    "speed_kph", "grade_pct", "roll_deg", "pitch_deg",
+    "gnss_hdop", "gnss_jitter_m",
+    "ground_firmness", "moisture_index", "visibility",
+    "blade_load_pct", "micro_corrections_per_min",
+]
